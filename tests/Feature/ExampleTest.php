102ac9ae0cde2db55d591c7d461332c0052e55ed
vadimlvov71 <57807117+vadimlvov71@users.noreply.github.com>
2021-04-29 20:26:14 +0300

    Add files via upload

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,22 +14,12 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-       // $response = $this->get('/');
-       // $this->assertEquals('302', $response->getStatusCode());
-        ////
-        $response = $this->withSession(['locale' => 'ua'])
-                         ->get('/');
+
+        $response = $this->withSession(['locale' => 'ua'])->get('/');
 		$this->assertEquals('302', $response->getStatusCode());
 		////
 		$response = $this->get('/ru/Slavskoe');
         $response->assertStatus(200);
-        ///
-        /* $this->browse(function (Browser $browser) {
-            $browser->visit('/ru/Slavskoe')
-                    //->assertSee('Славское')
-                    //->clickLink('Previous')
-                    //->assertPathIs('/alpha')
-                    ;
-        });*/
+       
     }
 }
