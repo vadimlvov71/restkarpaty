ef5fbb79695ed59afa47a2a1d3f6f3b2dcb20bb8
vadimlvov71 <57807117+vadimlvov71@users.noreply.github.com>
2021-04-30 19:00:06 +0300

    Add files via upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.app')
-
-@section('title', $title)
-<?php
-$local = App::getLocale();
-?>
-
+@section('title', __('lang.welcome'))
 
 @section('content')
- UPDATE dost SET content = REPLACE(content, 'http://vadimtur.ua/Karpaty/', 'http://vkarpatah.rest/')
-    <h3>Index</h3>
 	<?php
-	echo "locale".$locale."<br>";
+	$local = App::getLocale();
+	//echo "locale".$locale."<br>";
 	/*print_r($locale);
 	echo "</pre>";*/
 	/*echo "!!!!!<pre>";
@@ -19,19 +13,46 @@ $local = App::getLocale();
 	?>
 	
     <h1 class="display-4 text-center" style="font-size: 3.0rem">{{ __('lang.welcome')}}</h1>
-		@foreach ($products as $product)
-			<a href="{{url($locale.'/'.$product->translit)}}">{{ $product->name }}</a>
-		@endforeach
-		<div class="col-sm-12">hotels
-			<div class="row">
-				@foreach ($hotels as $item)
+    <div class="col-sm-12"><h2 class="h_header"> {{ __('lang.curorts')}}</h2>
+		<div class="row">
+			@foreach ($products as $product)
+				<div class="col-sm-3">
+					<a href="{{url($locale."/".$product->translit)}}">
+						<div class="hotel-section marginSection shadow">	
+							<div class="hotelHeader">{{$product->name}}</div>
+							<div class="img1">
+								<img align="center"  class="hotel_image img-fluid-height" src="{{url($image_path.'/'.$product->smallfoto)}}" alt="{{$product->name}}">
+							</div>
+							
+							<div class="hoteladditionalInfo">
+									
+							</div>
+						</div>
+					</a>
+				</div>
+			@endforeach
+		</div>
+	</div>
+	
+    <div class="col-sm-12"><h3 class="h_header">{{ __('lang.hotel_types')}}</h3>
+		<div class="row">
+			@foreach ($hotel_types as $hotel_type)
+				<div class="item_rest_new">
+					<a href="{{url($locale.'/'.$hotel_type->url)}}">{{ $hotel_type->name }}</a>
+				</div>
+			@endforeach
+		</div>
+	</div>
+	<div class="col-sm-12"><h4 class="h_header">{{ __('lang.hotels')}}</h4>
+		<div class="row">
+			@foreach ($hotels as $item)
 				@php
 					$product->translit = $item->translit;
 				@endphp
-					@include('commons.catalogs')
-				@endforeach
-			</div>
+				@include('commons.catalogs')
+			@endforeach
 		</div>
+	</div>
 @endsection
 
 
