bf85a831fefb88738c35f5e5263390c7b3232cb8
vadimlvov71 <57807117+vadimlvov71@users.noreply.github.com>
2021-04-30 19:01:20 +0300

    Add files via upload

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -9,7 +9,7 @@ use App\Helpers\Helper;
 use Illuminate\Support\Facades\Route;
 use Session;
 use URL; 
-
+use Lang;
 class CatalogController extends Controller
 {
 	
@@ -22,16 +22,13 @@ class CatalogController extends Controller
 			->where('translit', $get_catalog)
 			->first()
 		;
-		/*echo "<pre>";
-		print_r($request);
-		echo "</pre>";*/
-		$menu_rubrics = Helper::rubricMenu($product->name);
+		
 		$hotel_types = DB::select('select * from hotel_type where karpaty = ? ', ['yes']);
 		//App::singleton('data', function() { return array('abc' => 1); });
 		//App::singleton('product', function($product) { return ($product); });
 		$locale = Session::get('locale');
 		view()->share('hotel_types', $hotel_types);
-        view()->share('menu_rubrics', $menu_rubrics);
+        //view()->share('menu_rubrics', $menu_rubrics);
     }
     
 	public function index(Request $request){
@@ -44,7 +41,10 @@ class CatalogController extends Controller
 		echo "</pre>";*/
 		//$catalog = DB::select('select * from products where translit = ?', [$get_catalog]);
 		$product = DB::table('products')
-			->select('*')
+		->select([DB::raw('id_product, smallfoto, name, rest, translit, SUBSTRING_INDEX(description, " ", 20) as description'),
+					//DB::raw('*')
+					])
+			//->select('*')
 			->where('translit', $get_catalog)
 			->first()
 		;
@@ -70,7 +70,7 @@ class CatalogController extends Controller
 			->join('products', 'products.id_product', '=', 'doma.id_product')
 			->where('doma.id_product', $id_product)
 			->where('doma.sektor', '1')
-			->whereNotIn('gold', $notInGold)
+			->whereNotIn('gold', $this->notInGold)
 			//->where($getHotelTypeQuery[0], $getHotelTypeQuery[1])
 			->inRandomOrder()
 			->get()
@@ -81,9 +81,22 @@ class CatalogController extends Controller
 		$articles = DB::select('select * from article where id_catalog = ?', ['1']);
 		$title = "Catalog";
 		$locale = $this->getLocale($request);
-		$this->breadcrumbs[] = array('text' => $product->name);
-		//$data = App::make('data');
-        return view('catalog/index', ['title' => $title, 'product' => $product, 'products' => $products, 'dosts' => $dosts, 
+		//App::setLocale($locale);
+		//////menu_rubrics/////
+		if($locale == "ua"){
+			$title = Lang::trans('lang.catalogs_rest')[$id_product];
+			$title_menu = Lang::trans('lang.catalogs')[$id_product];
+		}else{
+			$title = $product->rest;
+			$title_menu = $product->name;
+		}
+		$menu_rubrics = Helper::rubricMenu($title_menu);
+		///end//menu_rubrics///
+		$lang_title = Lang::trans('lang.welcome');
+		$this->breadcrumbs[] = array('url' => '/', 'text' => $lang_title);
+		$this->breadcrumbs[] = array('text' => $title);
+
+        return view('catalog/index', ['title' => $title, 'title_menu'=> $title_menu, 'product' => $product, 'products' => $products, 'dosts' => $dosts, 'menu_rubrics' => $menu_rubrics,
         'articles' => $articles, 'hotels' => $hotels, 'privates' => $privates, 'breadcrumbs' => $this->breadcrumbs, 'locale' => $locale]);     
     }
     public function hoteltype(Request $request){
@@ -117,11 +130,25 @@ class CatalogController extends Controller
 		$articles = DB::select('select * from article where id_catalog = ?', ['1']);
 		//
 		$locale = $this->getLocale($request);
+		//App::setLocale($locale);
+		
+		$lang_title = Lang::trans('lang.welcome');
+		//////menu_rubrics/////
+		if($locale == "ua"){
+			$title = Lang::trans('lang.catalogs_rest')[$id_product];
+			$title_menu = Lang::trans('lang.catalogs')[$id_product];
+		}else{
+			$title = $product->rest;
+			$title_menu = $product->name;
+		}
+		$menu_rubrics = Helper::rubricMenu($title_menu);
+		///end//menu_rubrics///
 		$breadcrumbs_name = Helper::localeType($hotel_type, $locale);
+		$this->breadcrumbs[] = array('url' => '/', 'text' => $lang_title);
 		$this->breadcrumbs[] = array('url' => $product->translit, 'text' => $product->name);
 		$this->breadcrumbs[] = array( 'text' => $breadcrumbs_name);
-        return view('catalog/hotel_type', ['product' => $product, 'products' => $products,  'dosts' => $dosts, 'articles' => $articles,
-         'hotels' => $hotels, 'hotel_type' => $hotel_type, 'breadcrumbs' => $this->breadcrumbs, 'locale' => $locale]);      
+        return view('catalog/hotel_type', ['product' => $product, 'products' => $products,  'dosts' => $dosts, 'articles' => $articles, 'test' => $this->notInGold, 'menu_rubrics' => $menu_rubrics,
+         'hotels' => $hotels, 'hotel_type' => $hotel_type, 'breadcrumbs' => $this->breadcrumbs, 'locale' => $locale, $id_product => $id_product]);      
     }
 	public function info(Request $request){
 		$get_catalog = $request->route()->parameter('get_catalog');
@@ -133,17 +160,48 @@ class CatalogController extends Controller
 		$id_product = $product->id_product;
 		$products = DB::select('select * from products where id_catalog = ?', ['1']);
 		
-		$hotels = DB::select('select * from doma where id_product = ? AND sektor = ?', [$id_product, 0]);
-		$privates = DB::select('select * from doma where id_product = ? AND sektor = ?', [$id_product, 1]);
+		$hotels = DB::table('doma')
+			->select('doma.name', 'translit', 'sektor', 'doma_id', 'hotel_translit', 'products.name as pname', 'content')
+			->join('products', 'products.id_product', '=', 'doma.id_product')
+			->where('doma.id_product', $id_product)
+			->where('doma.sektor', 1)
+			->whereNotIn('gold', $this->notInGold)
+			//->where($getHotelTypeQuery[0], $getHotelTypeQuery[1])
+			->inRandomOrder()
+            ->limit(8)
+			->get()
+		;
+		$privates = DB::table('doma')
+			->select('doma.name', 'translit', 'sektor', 'doma_id', 'hotel_translit', 'products.name as pname')
+			->join('products', 'products.id_product', '=', 'doma.id_product')
+			->where('doma.id_product', $id_product)
+			->where('doma.sektor', '1')
+			->whereNotIn('gold', $this->notInGold)
+			//->where($getHotelTypeQuery[0], $getHotelTypeQuery[1])
+			->inRandomOrder()
+            ->limit(8)
+			->get()
+		;
 		$hotel_types = DB::select('select * from hotel_type where karpaty = ? ', ['yes']);
 		$dosts = DB::select('select * from dost where id_product = ? ', [$id_product]);
 		$articles = DB::select('select * from article where id_catalog = ?', ['1']);
 		/////
 		$title = "Info";
 		$locale = $this->getLocale($request);
+		//////menu_rubrics/////
+		if($locale == "ua"){
+			$title = Lang::trans('lang.catalogs_rest')[$id_product];
+			$title_menu = Lang::trans('lang.catalogs')[$id_product];
+		}else{
+			$title = $product->rest;
+			$title_menu = $product->name;
+		}
+		$menu_rubrics = Helper::rubricMenu($title_menu);
+		///end//menu_rubrics///
+		//App::setLocale($locale);
 		$this->breadcrumbs[] = array('url' => $product->translit, 'text' => $product->name);
 		$this->breadcrumbs[] = array('text' => $title);
-        return view('catalog/info', ['title' => $title, 'privates' => $privates, 'product' => $product, 'products' => $products, 'dosts' => $dosts, 'articles' => $articles,
+        return view('catalog/info', ['title' => $title, 'privates' => $privates, 'product' => $product, 'products' => $products, 'dosts' => $dosts, 'articles' => $articles, 'test' => $id_product, 'menu_rubrics' => $menu_rubrics,
          'hotels' => $hotels, 'breadcrumbs' => $this->breadcrumbs, 'locale' => $locale]);      
     }
     public function dosts(Request $request){
@@ -156,19 +214,48 @@ class CatalogController extends Controller
 		$id_product = $product->id_product;
 		$products = DB::select('select * from products where id_product = ?', ['1']);
 		
-		$hotels = DB::select('select * from doma where id_product = ? AND sektor = ?', [$id_product, 0]);
-		$privates = DB::select('select * from doma where id_product = ? AND sektor = ?', [$id_product, 1]);
+		$hotels = DB::table('doma')
+			->select('doma.name', 'translit', 'sektor', 'doma_id', 'hotel_translit', 'products.name as pname')
+			->join('products', 'products.id_product', '=', 'doma.id_product')
+			->where('doma.id_product', $id_product)
+			->where('doma.sektor', '0')
+			->whereNotIn('gold', $this->notInGold)
+			//->where($getHotelTypeQuery[0], $getHotelTypeQuery[1])
+			->inRandomOrder()
+			->get()
+		;
+		$privates = DB::table('doma')
+			->select('doma.name', 'translit', 'sektor', 'doma_id', 'hotel_translit', 'products.name as pname')
+			->join('products', 'products.id_product', '=', 'doma.id_product')
+			->where('doma.id_product', $id_product)
+			->where('doma.sektor', '1')
+			->whereNotIn('gold', $this->notInGold)
+			//->where($getHotelTypeQuery[0], $getHotelTypeQuery[1])
+			->inRandomOrder()
+			->get()
+		;
 		$hotel_types = DB::select('select * from hotel_type where karpaty = ? ', ['yes']);
 		$dosts = DB::select('select * from dost where id_product = ? ', [$id_product]);
 		$articles = DB::select('select * from article where id_catalog = ?', ['1']);
 		/////
 		
 		$locale = $this->getLocale($request);
+		App::setLocale($locale);
+		//////menu_rubrics/////
+		if($locale == "ua"){
+			$title = Lang::trans('lang.catalogs_rest')[$id_product];
+			$title_menu = Lang::trans('lang.catalogs')[$id_product];
+		}else{
+			$title = $product->rest;
+			$title_menu = $product->name;
+		}
+		$menu_rubrics = Helper::rubricMenu($title_menu);
+		///end//menu_rubrics///
 		$breadcrumbs_name = Helper::localeType("dosts", $locale);
 		$title = $breadcrumbs_name;
 		$this->breadcrumbs[] = array('url' => $product->translit, 'text' => $product->name);
 		$this->breadcrumbs[] = array('text' => $title);
         return view('catalog/dosts', ['title' => $title, 'privates' => $privates, 'product' => $product, 'products' => $products, 'dosts' => $dosts, 'articles' => $articles,
-         'hotels' => $hotels, 'breadcrumbs' => $this->breadcrumbs, 'locale' => $locale]);      
+         'hotels' => $hotels, 'breadcrumbs' => $this->breadcrumbs, 'locale' => $locale, 'menu_rubrics' => $menu_rubrics]);      
     }
 }
