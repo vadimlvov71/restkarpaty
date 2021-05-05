6ae4035f31a3a282ef7b9854ba1e2c6684166d5a
vadimlvov71 <57807117+vadimlvov71@users.noreply.github.com>
2021-05-05 11:32:52 +0300

    Add files via upload

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -55,19 +55,19 @@ class Helper
 		}else if($type_hotel == "sauna_banya_hotel"){
 			$paraQuery[0] = "spa";
 			$paraQuery[1] =  "1";
-		}else if($type_hotel == "Bassein"){
+		}else if($type_hotel == "bassein"){
 			$paraQuery[0] = "bassein";
 			$paraQuery[1] =  "1";
-		}else if($type_hotel == "Children"){
+		}else if($type_hotel == "children"){
 			$paraQuery[0] = "deti_more";
 			$paraQuery[1] =  "1";
-		}else if($type_hotel == "Rybalka"){
+		}else if($type_hotel == "rybalka"){
 			$paraQuery[0] = "rybalka";
 			$paraQuery[1] =  "1";
-		}else if($type_hotel == "Beach"){
+		}else if($type_hotel == "beach"){
 			$paraQuery[0] = "plyazh";
 			$paraQuery[1] =  "1";
-		}else if($type_hotel == "Korporative"){
+		}else if($type_hotel == "korporative"){
 			$paraQuery[0] = "corporative";
 			$paraQuery[1] =  "1";	
 		}else if($type_hotel == "mini_hoteli"){
@@ -104,8 +104,95 @@ class Helper
 		}
 		return $paraQuery;
 	}
+	public static function getHotelTypes($type, $type_number, $link_type="plural"){
+		$result = array();
+		if($type == "sektor" && $type_number == "1"){
+			if($link_type == "single"){
+				$private = "Частный сектор".self::$kurort;
+			}else{
+				$private = "Частный сектор";
+			}
+			$result["private_sector"] = $private;
+		}else{
+			if($link_type == "single"){
+				$guest = "Гостевой дом";
+				$pansionati = "Пансионат".self::$kurort;
+				$base = "База отдыха".self::$kurort;
+				$mini = "Мини-гостиница".self::$kurort;
+				$nedorogie = "Недорогая гостиница".self::$kurort;
+				$comfortable = "Комфортабельная гостиница".self::$kurort;
+			}else{
+				$guest = "Гостевые дома";
+				$pansionati = "Пансионаты";
+				$base = "Базы отдыха";
+				$mini = "Мини-гостиницы";
+				$nedorogie = "Недорогие гостиницы";
+				$comfortable = "Комфортабельные гостиницы";
+			}
+			//echo "type".$type."<br>";
+			//echo "type_number".$type_number."<br>";
+			if($type == "type"){
+				if($type_number == "12"){
+					$result["gostevye_doma"] = "Гостевые дома";
+				}else if($type_number == "2"){
+					$result["pansionati"] = $pansionati;
+				}else if($type_number == "3"){
+					$result["bazi_otdyha"] = $base;
+				}else if($type_number == "6"){
+					$result["mini_hoteli"] = $mini;
+				}
+			}else if($type == "spa" ){
+				if($type_number == "1"){
+					$result["spa"] = "Гостиница с сауной или баней";
+				}else{
+					$result = "no";
+				}
+			}else if($type == "deti_more" ){
+				if($type_number == "1"){
+					$result["children"] = "Отдых с детьми на море";
+				}else{
+					$result = "no";
+				}	
+			}else if($type == "plyazh"){
+				if($type_number == "1"){
+					$result["beach"] = "Гостиница со своим пляжем";
+				}else{
+					$result = "no";
+				}	
+			}else if($type == "corporative"){
+				if($type_number == "1"){
+					$result["korporative"] = "Корпоратив на море";
+				}else{
+					$result = "no";
+				}	
+			}else if($type == "bassein"){
+				if($type_number == "1"){
+					$result["bassein"] = "Гостиница с бассейном";
+				}else{
+					$result = "no";
+				}	
+			}else if($type == "sector"){
+				if($type_number == "1"){
+					$result["vip_rest"] = "VIP отдых";
+				}else if($type_number == "2"){
+					$result["comfortable-hotels"] = $comfortable;
+				}else if($type_number == "3"){
+					$result["nedorogie-hotels"] = $nedorogie;
+				}	
+			}
+		}
+		return $result;
+	}
 	public static function getHotelTypeUrl($type){
 		$replace = array("0" => "hotels", "1" => "private_sector");
 		return $replace[$type];
+	}
+	public static function getHotelTypeName($sektor, $locale){
+		if($locale == 'ua'){
+			$replace = array("0" => "готелі", "1" => "приватний сектор");
+		}else{
+			$replace = array("0" => "гостиницы", "1" => "частный сектор");
+		}
+		return $replace[$sektor];
 	}
 }
